8db18c53d720df73358fd4b5d70b5b5daad61e1b
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-08 22:00:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,19 @@ attr_reader :id
     new_stedent.grade = row[2]
     new_song  
 end
+
+def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
  
 
 end
